b8a7ca5e07fa0e832dac1b5fe57ef2de94fb9c89
burzenn <burzenn@gmail.com>
2024-03-21 00:51:08 +0300

    done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,22 +6,56 @@ public class Main {
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+
+        if (isBarking && 8 <= clock && clock <= 20) {
+            return true;
+        } else if (clock < 0 || clock > 23){
+            return false;
+        } else {
+            return false;
+        }
+
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
+
+        if (13 < firstAge && firstAge <=19 || 13 < secondAge && secondAge <19 || 13 < thirdAge && thirdAge <19) {
+            return true;
+        } else {
+            return false;
+        }
        
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
+
+        if (isSummer && 25 <= temp && temp <= 35) {
+            return true;
+        } else if (!isSummer && 25 <= temp && temp <= 45) {
+            return true;
+        } else {
+            return false;
+        }
      
     }
 
     public static double area(double width, double height) {
-       
+
+        double result = width * height ;
+        if (width < 0 || height < 0) {
+            return -1 ;
+        } else {
+            return result;
+        }
     }
 
     public static double area(double radius) {
-       
+
+        double result = radius*radius*Math.PI ;
+        if (radius < 0) {
+            return -1 ;
+        } else {
+            return result;
+        }
     }
 }
